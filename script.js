d6628f841b00b29f29ee12cec55acb99d421b6f4
S460 <aleksandar.ta6ev@gmail.com>
2020-11-11 19:42:55 +0200

    Added the ability to enter decimal numbers and delete single numbers

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function add(num1, num2) {
   return num1 + num2;
 }
-function subbtract(num1, num2) {
+function subtract(num1, num2) {
   return num1 - num2;
 }
 function multiply(num1, num2) {
@@ -11,6 +11,7 @@ function divide(num1, num2) {
 
   return Math.round((num1 / num2) * 10 ** 6) / 10 ** 6;
 }
+
 function operate(sign, num1, num2) {
   switch (true) {
     case sign === "*":
@@ -20,7 +21,7 @@ function operate(sign, num1, num2) {
     case sign === "+":
       return add(num1, num2);
     case sign === "-":
-      return subbtract(num1, num2);
+      return subtract(num1, num2);
 
     default:
       break;
@@ -38,7 +39,6 @@ function displayNumbers() {
 
   buttons.forEach((button) => {
 
-
     button.addEventListener("click", () => {
       if (button.value === "clear") {
         num2 = undefined
@@ -53,14 +53,23 @@ function displayNumbers() {
 
           displayText.textContent = operate(sign, +num1, +num2)
           num1 = displayText.textContent
-          num2 = undefined
-          sign = undefined
+          num2 = sign = undefined
           console.log(sign, num2, num1)
         }
-
-
-        //        num2 = undefined
       }
+      else if (button.value == "ce") {
+        len = displayText.textContent.length
+        num1 = displayText.textContent = displayText.textContent.slice(0, len - 1)
+
+        console.log(num1, num2, sign)
+
+      }
+      else if (button.value == ".") {
+        if ((displayText.textContent.indexOf(".") < 0)) {
+          displayText.textContent += "."
+        }
+      }
+
 
       else if (!+button.value) {
         sign = button.value
@@ -71,10 +80,12 @@ function displayNumbers() {
 
         if (sign !== undefined) {
 
-          num2 === undefined ? displayText.textContent = "" : console.log("good")
+          if (num2 === undefined) {
+            displayText.textContent = ""
+          }
           displayText.textContent += button.value
           num2 = displayText.textContent
-          console.log(num1, num2, sign + "s=u")
+          console.log(num1, num2, sign)
 
         }
 
@@ -90,4 +101,3 @@ function displayNumbers() {
 
 displayNumbers();
 
-console.log(7 - 5)
