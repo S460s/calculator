f803e0269d77a03faca7c02632c2e5cb395cbf07
S460 <aleksandar.ta6ev@gmail.com>
2020-11-11 20:20:55 +0200

    Added if satement to handel division by 0

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ function multiply(num1, num2) {
   return num1 * num2;
 }
 function divide(num1, num2) {
-
+  if (num2 === 0) {
+    alert("WARNING: You can not divide by zero!")
+    return ""
+  }
   return Math.round((num1 / num2) * 10 ** 6) / 10 ** 6;
 }
 
@@ -36,6 +39,7 @@ function displayNumbers() {
   let num1
   let num2
   let sign
+  let possibleSigns = ["*", "/", "+", "-"]
 
   buttons.forEach((button) => {
 
@@ -72,7 +76,7 @@ function displayNumbers() {
       }
 
 
-      else if (!+button.value) {
+      else if (possibleSigns.includes(button.value)) {
         sign = button.value
         console.log(sign, num1, num2)
       }
