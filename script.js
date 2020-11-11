177aa492e870b4646c472a2ad38a2149982e4cce
S460 <aleksandar.ta6ev@gmail.com>
2020-11-11 13:41:47 +0200

    Added functionality to the buttons

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
 function add(num1, num2) {
-  return num1 + num2
+  return num1 + num2;
 }
 function subbtract(num1, num2) {
-  return num1 - num2
+  return num1 - num2;
 }
 function multiply(num1, num2) {
-  return num1 * num2
+  return num1 * num2;
 }
 function divide(num1, num2) {
-  return num1 / num2
+  return num1 / num2;
 }
 function operate(sign, num1, num2) {
   switch (true) {
     case sign === "*":
-      return multiply(num1, num2)
+      return multiply(num1, num2);
     case sign === "/":
-      return divide(num1, num2)
+      return divide(num1, num2);
     case sign === "+":
-      return add(num1, num2)
+      return add(num1, num2);
     case sign === "-":
-      return divide(num1, num2)
-
+      return divide(num1, num2);
 
     default:
       break;
   }
 }
+
+let display = document.querySelector("#display");
+let displayText = document.querySelector("#displayText");
+let displayValue = [];
+let buttons = document.querySelectorAll("button");
+
+function displayNumbers() {
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      displayText.textContent = button.value;
+    });
+  });
+}
+
+displayNumbers();
