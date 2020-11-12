bae12c070b398f195d283fde9447c621ee08e9af
S460 <aleksandar.ta6ev@gmail.com>
2020-11-12 11:35:20 +0200

    Separated the calculator logic from the display function.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let display = document.querySelector("#display");
+let displayText = document.querySelector("#displayText");
+let buttons = document.querySelectorAll("button");
+let num1
+let num2
+let sign
+
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -12,7 +20,7 @@ function divide(num1, num2) {
     alert("WARNING: You can not divide by zero!")
     return ""
   }
-  return Math.round((num1 / num2) * 10 ** 6) / 10 ** 6;
+  return Math.round((num1 / num2) * 10 ** 12) / 10 ** 12;
 }
 
 function operate(sign, num1, num2) {
@@ -31,88 +39,81 @@ function operate(sign, num1, num2) {
   }
 }
 
-let display = document.querySelector("#display");
-let displayText = document.querySelector("#displayText");
-let buttons = document.querySelectorAll("button");
 
-function displayNumbers() {
-  let num1
-  let num2
-  let sign
+
+function calculatorLogic(char) {
+
+
   let possibleSigns = ["*", "/", "+", "-"]
+  if (char === "clear") {
+    num2 = undefined
+    num1 = undefined
+    sign = undefined
+    displayText.textContent = ""
+  }
+  else if (char === "=") {
 
-  buttons.forEach((button) => {
+    if (num1 != undefined && num2 != undefined && sign != undefined) {
 
-    button.addEventListener("click", () => {
-      if (button.value === "clear") {
-        num2 = undefined
-        num1 = undefined
-        sign = undefined
+      displayText.textContent = operate(sign, +num1, +num2)
+      num1 = displayText.textContent
+      num2 = sign = undefined
+      console.log(sign, num2, num1)
+    }
+  }
+  else if (char == "ce") {
+    len = displayText.textContent.length
+    num1 = displayText.textContent = displayText.textContent.slice(0, len - 1)
+
+    console.log(num1, num2, sign)
+
+  }
+  else if (char == ".") {
+    if ((displayText.textContent.indexOf(".") < 0)) {
+      displayText.textContent += "."
+    }
+  }
+  else if (possibleSigns.includes(char)) {
+    sign = char
+    console.log(sign, num1, num2)
+  }
+
+  else if (char) {
+
+    if (sign !== undefined) {
+
+      if (num2 === undefined) {
         displayText.textContent = ""
       }
 
-
-      else if (button.value === "=") {
-
-        if (num1 != undefined && num2 != undefined && sign != undefined) {
-
-          displayText.textContent = operate(sign, +num1, +num2)
-          num1 = displayText.textContent
-          num2 = sign = undefined
-          console.log(sign, num2, num1)
-        }
+      if (displayText.textContent.length > 15) {
+        alert("You can't enter more numbers.")
       }
-      else if (button.value == "ce") {
-        len = displayText.textContent.length
-        num1 = displayText.textContent = displayText.textContent.slice(0, len - 1)
-
-        console.log(num1, num2, sign)
-
-      }
-      else if (button.value == ".") {
-        if ((displayText.textContent.indexOf(".") < 0)) {
-          displayText.textContent += "."
-        }
-      }
-
-
-      else if (possibleSigns.includes(button.value)) {
-        sign = button.value
-        console.log(sign, num1, num2)
-      }
-
       else {
-
-        if (sign !== undefined) {
-
-          if (num2 === undefined) {
-            displayText.textContent = ""
-          }
-
-          if (displayText.textContent.length > 15) {
-            alert("You can't enter more numbers.")
-          }
-          else {
-            displayText.textContent += button.value
-            num2 = displayText.textContent
-            console.log(num1, num2, sign)
-          }
-        }
-
-        else if (displayText.textContent.length > 15) {
-          alert("You can't enter more numbers.")
-        }
-
-
-        else {
-
-          displayText.textContent += button.value;
-          num1 = displayText.textContent
-          console.log(num1, num2)
-        }
+        displayText.textContent += char
+        num2 = displayText.textContent
+        console.log(num1, num2, sign)
       }
-    });
-  });
+    }
+
+    else if (displayText.textContent.length > 15) {
+      alert("You can't enter more numbers.")
+    }
+    else {
+      displayText.textContent += char;
+      num1 = displayText.textContent
+      console.log(num1, num2)
+    }
+  }
 }
 
-displayNumbers();
+
+function displayNubers() {
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      calculatorLogic(button.value)
+    })
+  });
+}
+displayNubers()
